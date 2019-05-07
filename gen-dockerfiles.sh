3644ac5866ecbcec902a5f6a0dc45da9293c03ce
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-05-07 12:26:44 -0400

    Change filepath of gen script.

--- gen-dockerfiles.sh
+++ gen-dockerfiles.sh
@@ -4,15 +4,15 @@
 # Registry will be ignored for now unless we move off Docker Hub.
 
 # Import repo specific image information
-source ../manifest
+source ./manifest
 
 version=( "$1" )  # SemVer version number passed in via command-line
 sha=( "$2" )  # SHA hash for the main binary of the image, passed in via command-line
 tagless_image=circleciimages/${repository}
 
 # prepare file
-echo "#!/usr/bin/env bash" > ../build-images.sh
-echo "" >> ../build-images.sh
+echo "#!/usr/bin/env bash" > ./build-images.sh
+echo "" >> ./build-images.sh
 
 string="docker build"
 
@@ -25,9 +25,9 @@ fi
 
 [[ -d "$versionShort" ]] || mkdir "$versionShort"
 
-sed -r -e 's!%%PARENT%%!'"$parent"'!g' "../Dockerfile.template" > "../$versionShort/Dockerfile"
-sed -i 's/%%GO_VERSION%%/'"${version}"'/g' "../${versionShort}/Dockerfile"
-sed -i 's!%%GO_SHA%%!'"$sha"'!g' "../$versionShort/Dockerfile"
+sed -r -e 's!%%PARENT%%!'"$parent"'!g' "./Dockerfile.template" > "./$versionShort/Dockerfile"
+sed -i 's/%%GO_VERSION%%/'"${version}"'/g' "./${versionShort}/Dockerfile"
+sed -i 's!%%GO_SHA%%!'"$sha"'!g' "./$versionShort/Dockerfile"
 
 string="$string --file $versionShort/Dockerfile"
 if [[ $i == 0 ]]; then
@@ -42,4 +42,4 @@ fi
 
 string="$string ."
 
-echo "$string" >> ../build-images.sh
+echo "$string" >> ./build-images.sh
--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ for i in "${!sortedVersions[@]}"; do
 	version=${sortedVersions[$i]}
 
 	git checkout -b release-v${version}
-	./gen-dockerfiles.sh ${version} fake-sha
+	shared/gen-dockerfiles.sh ${version} fake-sha
 	git add .
 	git commit -m "Add v${version}."
 	git push -u origin release-v${version}
